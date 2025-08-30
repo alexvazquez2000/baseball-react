76682f13812adc57602b88492a5b2ce654a6cbcf
Alex Vazquez <101617394+alexvazquez2000@users.noreply.github.com>
2025-08-30 12:55:31 -0600

    Footer with hard-coded year
    Need to make the year dynamic - no need for react, but it should not be
    hardcoded

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render () {
+        return (
+            <footer class="bg-light text-center text-lg-start mt-4 ">
+              <div class="text-center p-3">
+                <p>&copy; 2025 NE Baseball League. All rights reserved.</p>
+              </div>
+            </footer>
+        );
+    }
+}
+
+export default Footer;
